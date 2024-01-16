bf564d6c5b397c04b645e08c639915b81f8e538f
idoko <okokomichaels@outlook.com>
2024-01-16 15:42:20 +0100

    problem: httpc cannot follow redirects
    solution: As a first setup, setup base tests for redirects

--- extensions/omni_httpc/src/http_request.sql
+++ extensions/omni_httpc/src/http_request.sql
@@ -2,7 +2,8 @@ create function http_request(
     url text,
     method omni_http.http_method default 'GET',
     headers omni_http.http_headers default array []::omni_http.http_headers,
-    body bytea default null) returns http_request as
+    body bytea default null,
+    follow_redirect boolean default false) returns http_request as
 $$
 select row (method, url, headers, body)
 $$
